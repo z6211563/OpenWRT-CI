bd09d1733be9c047b94b9e5cb0af0fa98d60dcc3
z6211563 <z6211563@gmail.com>
2025-02-07 17:55:47 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -39,7 +39,7 @@ UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 UPDATE_PACKAGE "lazyoop" "lazyoop/networking-artifact" "main"
 
 UPDATE_PACKAGE "luci-app-wechatpush" "tty228/luci-app-wechatpush" "master"
-UPDATE_PACKAGE "luci-app-nezha-v1" "dysf888/OpenWRT_NeZha_V1" "main" "pkg"
+UPDATE_PACKAGE "nezha" "dysf888/OpenWRT_NeZha_V1" "main"
 
 if [[ $WRT_REPO != *"immortalwrt"* ]]; then
 	UPDATE_PACKAGE "qmi-wwan" "immortalwrt/wwan-packages" "master" "pkg"
